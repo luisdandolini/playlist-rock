41153079dc1968204e4e2242fa7786f116b84b1f
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2023-01-09 15:38:18 -0300

    feat: add playlist third

--- src/App.js
+++ src/App.js
@@ -4,40 +4,55 @@ function App() {
   return (
     <div className="container-app">
       <div className='playlist-first'>
-          <img className='music' src='/img/music.png' alt='Foto música'/>
-          <h2 className='music-name'>Acorda Devinho</h2>
-          <p className='band-name'>Banda Rocketseat</p>
-          <div>
-            <div className='controls'>
-              <img src='/img/play-back.png' alt='Ícone voltar música' />
-              <img src='/img/play.png' alt='Ícone play música' />
-              <img src='/img/play-forward.png' alt='Ícone avançar música' />
-            </div>
-            <img className='music-time' src='/img/time-music.png' alt='Tempo da música' />
-            <div className='music-minutes'>
-              <span>03:20</span> <span>00:12</span>
-            </div>
+        <img className='music' src='/img/music.png' alt='Foto música'/>
+        <h2 className='music-name'>Acorda Devinho</h2>
+        <p className='band-name'>Banda Rocketseat</p>
+        <div>
+          <div className='controls'>
+            <img src='/img/play-back.png' alt='Ícone voltar música' />
+            <img src='/img/play.png' alt='Ícone play música' />
+            <img src='/img/play-forward.png' alt='Ícone avançar música' />
           </div>
+          <img className='music-time' src='/img/time-music.png' alt='Tempo da música' />
+          <div className='music-minutes'>
+            <span>03:20</span> <span>00:12</span>
+          </div>
+        </div>
       </div>
 
       <div className='playlist-second'>
-          <div className='container-second'>
-            <img src='/img/music.png' alt='Foto música'/>
-            <div>
-              <h2>Acorda Devinho</h2>
-              <p>Banda Rocketseat</p>
-            </div>
-          </div>
+        <div className='container-second'>
+          <img src='/img/music.png' alt='Foto música'/>
           <div>
-            <div className='controls'>
-              <img src='/img/play-back.png' alt='Ícone voltar música' />
-              <img src='/img/play.png' alt='Ícone play música' />
-              <img src='/img/play-forward.png' alt='Ícone avançar música' />
-            </div>
-            <img className='music-time-second' src='/img/time-music.png' alt='Tempo da música' />
-            <div className='music-minutes-second'>
-              <span>03:20</span> <span>00:12</span>
-            </div>
+            <h2>Acorda Devinho</h2>
+            <p>Banda Rocketseat</p>
+          </div>
+        </div>
+        <div>
+          <div className='controls'>
+            <img src='/img/play-back.png' alt='Ícone voltar música' />
+            <img src='/img/play.png' alt='Ícone play música' />
+            <img src='/img/play-forward.png' alt='Ícone avançar música' />
+          </div>
+          <img className='music-time-second' src='/img/time-music.png' alt='Tempo da música' />
+          <div className='music-minutes-second'>
+            <span>03:20</span> <span>00:12</span>
+          </div>
+        </div>
+      </div>
+
+      <div className='playlist-third'>
+        <div className='container-second'>
+          <img src='/img/music.png' alt='Foto música'/>
+          <div>
+            <h2>Acorda Devinho</h2>
+            <p>Banda Rocketseat</p>
+          </div>
+        </div>
+        <div className='controls'>
+            <img src='/img/play-back.png' alt='Ícone voltar música' />
+            <img src='/img/play.png' alt='Ícone play música' />
+            <img src='/img/play-forward.png' alt='Ícone avançar música' />
           </div>
       </div>
     </div>
